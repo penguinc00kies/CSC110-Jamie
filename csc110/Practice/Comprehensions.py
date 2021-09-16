3ba96b8528898604f96e29df72e6e991f063ca76
penguincookies <jamiesumin@gmail.com>
2021-09-16 11:29:47 -0400

    Added tutorial work

--- csc110/Practice/Comprehensions.py
+++ csc110/Practice/Comprehensions.py
@@ -8,4 +8,8 @@ print([char * 2 for char in 'Jamie'])
 print([char + '!' for char in 'Mario'])
 print(1 in my_set)
 print({x: x ** 2 for x in my_set})
+print({x*2:x*3 for x in range (6)})
 print({x for x in range(30, 51)})
+set1 = {1, 2, 3}
+set2 = {4, 5, 6}
+print({(x,y) for x in set1 for y in set2})

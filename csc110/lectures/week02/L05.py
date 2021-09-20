a4ce75ee787dc942514808584b806163586aecef
penguincookies <jamiesumin@gmail.com>
2021-09-20 15:36:30 -0400

    Worksheet 5

--- csc110/lectures/week02/L05.py
+++ csc110/lectures/week02/L05.py
@@ -64,16 +64,44 @@ def taxed_total(order: dict, tax_rate: float) -> float:
 ####################################################################################################
 # TODO: Given three side lengths of a triangle (as floats), calculate the angles in the triangle.
 import math
-# def calculate_angles(side_a: float, side_b: float, side_c: float) -> tuple:
-    #"""Returns a tuple of 3 angles in degrees of a triangle given the triangle has side1, side2, and side3
+def calculate_angles(side_a: float, side_b: float, side_c: float) -> tuple:
+    """Returns a tuple of 3 angles in degrees of a triangle given the triangle has side_a, side_b, and side_c
 
-    #The angeles in the tuple are ordered by...
-    #>>> calculate_angles(2.0, 2.0, 3.0)
-    #{43.71931114, 43.71931114, 97.18075578}
-    #"""
-    # angle1 = (side_a**2 - side_b**2 - side_c**2)/(-2*side_b*side_c)
-    # return
+    The first angle in the tuple is the angle opposite of side_a, the second angle is opposite of side_b,
+    and the third angle is opposite of side_c
+    >>> calculate_angles(2.0, 2.0, 3.0)
+    (41.40962210927086, 41.40962210927086, 97.18075578145829)
+    """
+    angle_a = (math.acos((side_a**2 - side_b**2 - side_c**2)/(-2*side_b*side_c))) * 180.0 / math.pi
+    angle_b = (math.acos((side_b ** 2 - side_a ** 2 - side_c ** 2) / (-2 * side_a * side_c))) * 180.0 / math.pi
+    angle_c = (math.acos((side_c ** 2 - side_a ** 2 - side_b ** 2) / (-2 * side_a * side_b))) * 180.0 / math.pi
+    return (angle_a, angle_b, angle_c)
 
 ####################################################################################################
 # Additional exercises
 ####################################################################################################
+
+# TODO: Given a set of strings, calculate the length of the longest string.
+def longest_length(strings: set) -> int:
+    """Returns the longest length of a string in the set strings
+    
+    >>> longest_length({'University', 'of', 'Toronto'})
+    10
+    >>> longest_length({'John', 'Alexander', 'Macdonald'})
+    9
+    """
+    return max((len(x) for x in strings))
+
+# TODO: You are renting a car to make a road trip across Canada. The car rental company you plan to
+#  use charges a fee of $50 plus $15 per day you rent the car.
+import datetime
+def total_rental_fee(start_day: datetime.date, end_day: datetime.date) -> int:
+    """Returns the total fee of renting a car between start_day and end_day, the fee is $50 + $15
+    per day the car is rented
+
+    >>> total_rental_fee(datetime.date(2020, 9, 20), datetime.date(2020, 9, 20))
+    65
+    >>> total_rental_fee(datetime.date(2020, 9, 20), datetime.date(2020, 10, 20))
+    515
+    """
+    return ((end_day - start_day).days + 1) * 15 + 50

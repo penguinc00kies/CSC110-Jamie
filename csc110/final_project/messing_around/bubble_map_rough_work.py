5d24c0552e6d4287e22da331aafc0f6bf423324a
penguinc00kies <jamiesumin@gmail.com>
2021-12-09 22:30:57 -0500

    bubble map rough

--- csc110/final_project/messing_around/bubble_map_rough_work.py
+++ csc110/final_project/messing_around/bubble_map_rough_work.py
@@ -1,5 +1,3 @@
-"""wcwvwv"""
-
 """display a bubble map with color-coded bubbles corresponding to a state's political leaning
 and size proportionate to the increase in hate crimes"""
 
@@ -24,16 +22,23 @@ with open('../data/state_colour_data.csv') as file:
 
 coordinates = pd.read_csv('../data/2014_us_cities.csv')
 hate_crime_data = pd.read_csv('../data/hate_crime_data.csv')
+# instantiate a new column called colour, I just needed to assign it placeholder dummy values
 hate_crime_data['colour'] = hate_crime_data['US City']
+# instantiate a new column called lat, I just needed to assign it placeholder dummy values
 hate_crime_data['lat'] = hate_crime_data['US City']
+# instantiate a new column called lon, I just needed to assign it placeholder dummy values
 hate_crime_data['lon'] = hate_crime_data['US City']
 
+# searches coordinates for cities that are also in hate_crime_data, and then assigns the
+# corresponding latitudes and longitudes from coordinates to the matching city in hate_crime_data
 for i in range(hate_crime_data.__len__()):
     for j in range(coordinates.__len__()):
         if hate_crime_data['US City'][i] == coordinates['name'][j]:
             hate_crime_data['lat'][i] = coordinates['lat'][j]
             hate_crime_data['lon'][i] = coordinates['lon'][j]
 
+# assigns each city in hate_crime_data it's proper colour(ie, political leaning) by indexing the
+# dictionary state_colour
 for i in range(hate_crime_data.__len__()):
     # this line will throw a warning, but the code still works as indented
     hate_crime_data['colour'][i] = state_colour[hate_crime_data['US State'][i]]
@@ -45,16 +50,16 @@ fig.add_trace(go.Scattergeo(
     lat=hate_crime_data['lat'],
     text=hate_crime_data['US City'],
     marker=dict(
-        size=5,
+        size=15,
         color=hate_crime_data['colour'],
         line_color='rgb(40,40,40)',
         line_width=0.5,
         sizemode='area'
     ),
-    name=str(hate_crime_data['US City'])))
+    name='US City'))
 
 fig.update_layout(
-    title_text='US city colours<br>(Click legend to toggle traces)',
+    title_text='US City Colours<br>(Click legend to toggle traces)',
     showlegend=True,
     geo=dict(
         scope='usa',
